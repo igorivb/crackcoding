fd8eb2accad387829902a2dafe1ccd58ed4cecf9
Igor <i.burilo1@gmail.com>
2014-08-20 16:32:50 +0300

    Rod cutting.

--- Algorithms/src/main/java/com/dynamicprog/RodCutting3.java
+++ Algorithms/src/main/java/com/dynamicprog/RodCutting3.java
@@ -1,53 +1,108 @@
 package com.dynamicprog;
 
+
 /**
  * See Cornel: 15.1 Rod cutting
  */
 public class RodCutting3 {
 	
-	public static int cutRod(int[] prices, int len) {
+	public static int cutRodTopDown(int[] prices, int len) {
 		if (len > prices.length) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("len");
 		}
 		
 		Integer[] memory = new Integer[prices.length];
-		return cutRod(prices, len, memory);
+		return cutRodTopDown(prices, len, memory);
 	}
 	
-	static int cutRod(int[] prices, int len, Integer[] memory) {
+	private static int cutRodTopDown(int[] prices, int len, Integer[] memory) {
 		if (len == 0) {
 			return 0;
 		}
+		if (memory[len - 1] != null) {
+			return memory[len - 1];
+		}
 		
-		int max = Integer.MIN_VALUE;
-		
-		for (int i = 0; i < len; i ++) {
-			int val = prices[i];
-			
-			int subLen = len - i - 1;
-			if (subLen > 0) {
-				if (memory[subLen - 1] == null) {
-					memory[subLen - 1] = cutRod(prices, subLen, memory);
-				}				
-				val += memory[subLen - 1];	
-			}						 
-			
+		int max = Integer.MIN_VALUE;		
+		for (int i = 0; i < len; i ++) {			
+			int val = prices[i] + cutRodTopDown(prices, len - i - 1, memory);
 			max = Math.max(max, val);
 		}
+		memory[len - 1] = max;
 		
 		return max;
 	}
 	
-	public static void main(String[] args) {
-		//value for len = prices[len - 1]
-		int[] prices = new int[] {1, 5, 8, 9, 10, 17, 17, 20, 24, 30};	
+	/*
+	 * Contains solution.
+	 */
+	public static Object[] cutRodBottomUp(int[] prices, int len) {
+		int[] memory = new int[prices.length + 1];	 //memory[0] = 0
+		Integer[] solution = new Integer[prices.length + 1];
+		solution[0] = 0;
 		
-		//int[] prices = new int[] {3, 5};
+		for (int intLen = 1; intLen <= len; intLen ++) { 
+			int q = Integer.MIN_VALUE;
+			for (int i = 0; i < intLen; i ++) {
+				int val =  prices[i] + memory[intLen - i - 1];
+				if (q < val) {
+					q = val;
+					//remember best solution at current length
+					solution[intLen] = i + 1;
+				}
+				
+			}
+			memory[intLen] = q;
+		}
+				
+		return new Object[] {memory[len], solution};
+	}	
 	
+	
+	public static String printCutRodBottomUp(int[] prices, int len) {		
+		Object[] result = cutRodBottomUp(prices, len);
 		
-		for (int i = 0; i <= prices.length; i ++) {
-			int revenue = cutRod(prices, i);
-			System.out.printf("%2s: %3s\n", i, revenue);
-		}		
+		int revenue = (int) result[0];
+		Integer[] solutions = (Integer[]) result[1];
+		
+		StringBuilder strSolution = new StringBuilder();		
+		while (len > 0) {
+			if (strSolution.length() > 0) {
+				strSolution.append(", ");
+			}
+			strSolution.append(solutions[len]);
+			len = len - solutions[len];
+		}
+		
+		String str = String.format("Revenue: %2s, solution: [%s]", revenue, strSolution);
+		return str;		
+	}
+	
+
+	public static void main(String[] args) {
+
+		int[] prices = new int[] {1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+		
+		for (int i = 1; i <= prices.length; i ++) {
+		
+			System.out.printf("%2s: %s\n", i, printCutRodBottomUp(prices, i));
+			
+//			Object[] result = cutRodBottomUp(prices, i);
+//			int revenue = (int) result[0];
+//			
+//			Integer[] solutionIndexes = (Integer[]) result[1];
+//			List<Integer> solution = new ArrayList<>();
+//			for (int j = 0; j < solutionIndexes.length; j ++) {
+//				if (solutionIndexes[j] != null) {
+//					solution.add(prices[solutionIndexes[j]]);	
+//				}				
+//			}
+//			
+//			System.out.printf("%2s: %3s | %s\n", i, revenue, solution);
+		}
+		
+		
+			
+
 	}
 }
