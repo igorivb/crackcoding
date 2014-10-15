c159a2646584df061cfea22d1c1d762ee251e9a5
Igor Burilo <Igor.Burilo@dataart.com>
2014-10-15 10:31:54 +0300

    WaterTask

--- Algorithms/src/main/java/com/dynamicprog/WaterTask.java
+++ Algorithms/src/main/java/com/dynamicprog/WaterTask.java
@@ -2,6 +2,7 @@ package com.dynamicprog;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -49,7 +50,10 @@ public class WaterTask {
 		
 	}
 	
-	static List<Action> findPath(int b1, int b2, int expected) {
+	/**
+	 * Use depth first search approach
+	 */
+	public static List<Action> findPath(int b1, int b2, int expected) {
 		int waterB1 = 0; int waterB2 = 0;
 		List<Action> actionPath = new ArrayList<>();
 		List<Action> solution = new ArrayList<>();
@@ -143,7 +147,7 @@ public class WaterTask {
 		}
 	}
 	
-	public static void main(String[] args) {
+	public static void main1(String[] args) {
 		int b1 = 3; int b2 = 5; int expected = 4;
 		
 		List<Action> path = findPath(b1, b2, expected);		
@@ -179,6 +183,102 @@ public class WaterTask {
 			}
 									
 			System.out.printf("%15s, b1: %2s, b2: %2s%n", act, waterB1, waterB2);
+		}
+	}
+	
+	
+	//--------------------------------------------------------
+	//--------------------------------------------------------
+	
+	static class Node {
+		int waterB1; int waterB2;
+		Action action;
+		Node parent;
+		Node(int waterB1, int waterB2, Action action, Node parent) {
+			this.waterB1 = waterB1;
+			this.waterB2 = waterB2;
+			this.action = action;
+			this.parent = parent;
+		}	
+//		void add(Node node) {
+//			node.parent = this;
+//		}
+	}
+	
+	/**
+	 * Uses breadth first search. It is better than DFS approach.
+	 */
+	public static Node findPathWithTree(int b1, int b2, int expected) {
+		LinkedList<Node> queue = new LinkedList<>(); 		
+		queue.add(new Node(0, 0, null, null));
+		
+		Node result = null;
+		while (!queue.isEmpty()) {
+			Node node = queue.removeFirst();
+			
+			//check solution
+			if (expected == node.waterB1 || expected == node.waterB2) {				
+				result = node;
+				break;
+			}
+			
+			fillChildren(node, b1, b2, queue);			
+		}
+		
+		return result;
+	}
+	
+	static void fillChildren(Node node, int b1, int b2, LinkedList<Node> queue) {
+		//FILL_B1
+		if (node.waterB1 < b1) {
+			queue.add(new Node(b1, node.waterB2, Action.FILL_B1, node));
+		}
+		
+		//CLEAR_B1
+		if (node.waterB1 > 0) {
+			queue.add(new Node(0, node.waterB2, Action.CLEAR_B1, node));								
+		}
+		
+		//FROM_B1_TO_B2
+		if (node.waterB1 > 0 && node.waterB2 < b2) {
+			int diff = Math.min(b2 - node.waterB2, node.waterB1);
+			queue.add(new Node(node.waterB1 - diff, node.waterB2 + diff, Action.FROM_B1_TO_B2, node));												
+		}				
+		
+		//FILL_B2
+		if (node.waterB2 < b2) {
+			queue.add(new Node(node.waterB1, b2, Action.FILL_B2, node));								
+		}
+		
+		//CLEAR_B2
+		if (node.waterB2 > 0) {
+			queue.add(new Node(node.waterB1, 0, Action.CLEAR_B2, node));				
+		}
+		
+		//FROM_B2_TO_B1
+		if (node.waterB2 > 0 && node.waterB1 < b1) {
+			int diff = Math.min(b1 - node.waterB1, node.waterB2);
+			queue.add(new Node(node.waterB1 + diff, node.waterB2 - diff, Action.FROM_B2_TO_B1, node));									
+		}
+	}
+	
+	public static void main(String[] args) {
+		int b1 = 3; int b2 = 5; int expected = 4;
+		
+		Node result = findPathWithTree(b1, b2, expected);		
+		
+		//print solution 
+		List<Node> solution = new ArrayList<>();
+		while (result != null) {
+			solution.add(0, result);
+			result = result.parent;
+		}
+		
+		System.out.printf("\nSolution: %2s%n", solution.size());
+		
+		for (int i = 0; i < solution.size(); i ++) {
+			Node n = solution.get(i);
+			System.out.printf("%2s. %15s, b1: %2s, b2: %2s%n", i, n.action, n.waterB1, n.waterB2);
 		}
 	}
 }
